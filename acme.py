7fcd991d93ec72b041314ff33ae3a6cdf24cfcda
mark-morelos <makoimorelos@gmail.com>
2020-09-13 23:21:38 -0700

    final Sprint Challenge 1 files added

--- acme.py
+++ acme.py
@@ -2,7 +2,6 @@ import unittest
 import random
 from random import randint
 
-
 class Product():
     def __init__(self, name, price=10, weight=20, flammability=0.5, identifier=random.randint(1000000, 9999999)):
         self.name = name
@@ -10,35 +9,31 @@ class Product():
         self.weight = weight
         self.flammability = flammability
         self.identifier = identifier
-
+    
     def stealability(self):
-        self.stealability = self.price/self.weight
-        if self.stealability < 0.5:
-            print(f'Not so stealable...')
-        elif 0.5 <= self.stealability < 1:
+        s = self.price / self.weight
+        if s < 0.5:
+            print(f'Not so stealable')
+        elif (s >= 0.5) and (s < 1):
             print(f'Kinda stealable.')
         else:
             print(f'Very stealable!')
 
     def explode(self):
-        self.explode = self.flammability * self.weight
-        if self.explode < 10:
+        e = self.flammability * self.weight
+        if e < 10:
             print(f'...fizzle.')
-        elif 10 <= self.explode < 50:
+        elif (e >= 10) and (e < 50):
             print(f'...boom!')
         else:
             print(f'...BABOOM!!')
 
 class BoxingGlove(Product):
-    def __init__(self, name, price=10, weight=10, flammability=0.5, identifier=random.randint(1000000, 9999999)):
-        self.name = name 
-        self.price = price
-        self.weight = weight
-        self.flammability = flammability
-        self.identifier = identifier
-
+    def __init__(self, name):
+        super().__init__(name, weight=10)
+    
     def explode(self):
-        print(f"...it's a glove.")
+        print(f"it's a glove.")
     
     def punch(self):
         if self.weight < 5:
